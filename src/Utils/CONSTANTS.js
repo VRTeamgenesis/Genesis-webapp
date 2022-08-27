95a10c1d9f3121c5ad48a2b80f8a88379563c0e5
ashok-3704 <ashokkumar.d@zohocorp.com>
2022-08-28 01:06:00 +0530

    issue fixes related to authentication

--- src/Utils/CONSTANTS.js
+++ src/Utils/CONSTANTS.js
@@ -1,13 +1,5 @@
 const WEB3_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEI3Zjg0QmJDYzQxYjMyNTY3NTdiZThEMDU3Njg1NmQzMWIwMTRCODIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjE1MDUwMTEyMjgsIm5hbWUiOiJHZW5lc2lzRHJpdmUifQ.glVNNw_u6qMSK411urnyTx0V1iuzE4Dv3xMjPK0VO9M';
 const FILE_ENCRYPTKEY = '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5';
 
-const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDTpSs1-ewLQqnx6elcYm6sNR50IeYXz10",
-    authDomain: "test-bdf0c.firebaseapp.com",
-    projectId: "test-bdf0c",
-    storageBucket: "test-bdf0c.appspot.com",
-    messagingSenderId: "116123758394",
-    appId: "1:116123758394:web:7459c3fed632c2b8f46a03",
-    measurementId: "G-DMGXKD92HR"
-}
+
 export {WEB3_TOKEN,FILE_ENCRYPTKEY, FIREBASE_CONFIG}
